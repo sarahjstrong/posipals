98af94b5a164f866a1a62e7c9b86fdf2dcb3cfbf
sarahjstrong <artbysarahjessicastrong@gmail.com>
2024-04-12 14:21:34 -0400

    worked on message controller

--- java/src/main/java/com/techelevator/controller/MessageController.java
+++ java/src/main/java/com/techelevator/controller/MessageController.java
@@ -1,11 +1,13 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.MessageDao;
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Message;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -21,33 +23,87 @@ public class MessageController {
 
     @RequestMapping(path = "/messages", method = RequestMethod.GET)
     public List<Message> getMessages() {
-        return null;
+        try{
+            List<Message> messages = messageDao.getMessages();
+            if(messages.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No messages found");
+            } else {
+                return messages;
+            }
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "GET Request Failed");
+        }
     }
 
-    @RequestMapping(path = "/messages/strangers", method = RequestMethod.GET)
-    public List<Message> getMessagesFromStrangers() {
-        return null;
+    @RequestMapping(path = "/messages/strangers/{username}", method = RequestMethod.GET)
+    public List<Message> getMessagesFromStrangers(@RequestParam String username) {
+        try {
+            List<Message> messages = messageDao.getMessagesFromStrangers(username);
+            if(messages.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No messages found");
+            } else {
+                return messages;
+            }
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "GET Request Failed");
+        }
     }
 
     @RequestMapping(path = "/messages/{id}", method = RequestMethod.GET)
     public Message getMessageById(@RequestParam int id) {
-        return null;
+        try {
+            Message message = messageDao.getMessageById(id);
+            if(message == null) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No message found");
+            } else {
+                return message;
+            }
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "GET Request Failed");
+        }
     }
 
     @RequestMapping(path = "/messages/{username}", method = RequestMethod.GET)
     public List<Message> getMessagesByUsername(@RequestParam String username) {
-        return null;
+        try {
+            List<Message> messages = messageDao.getMessagesByUsername(username);
+            if (messages.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No messages found");
+            } else {
+                return messages;
+            }
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "GET Request Failed");
+        }
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "/messages", method = RequestMethod.POST)
     public Message addMessage(@Valid @RequestBody Message newMessage) {
-        return null;
+        try {
+            Message message = messageDao.addMessage(newMessage);
+            if (message == null) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error submitting message");
+            } else {
+                return message;
+            }
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "POST Request Failed");
+        }
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @RequestMapping(path = "/messages", method = HttpMethod.DELETE)
+    @RequestMapping(path = "/messages", method = RequestMethod.DELETE)
     public int deleteMessageById(@RequestParam int id) {
-        return 0;
+        try {
+            int rowsDeleted = messageDao.deleteMessageById(id);
+            if (rowsDeleted == 0) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error deleting message");
+            } else {
+                return rowsDeleted;
+            }
+        } catch (DaoException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "DELETE Request Failed");
+        }
     }
 }
--- java/src/main/java/com/techelevator/dao/MessageDao.java
+++ java/src/main/java/com/techelevator/dao/MessageDao.java
@@ -1,9 +1,10 @@
 package com.techelevator.dao;
 
 import com.techelevator.model.Message;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Component
 public interface MessageDao {
     List<Message> getMessages();
     List<Message> getMessagesFromStrangers(String currentUsername);

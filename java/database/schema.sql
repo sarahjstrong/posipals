1d70f64e8839f9b1cf5e6a68d6b53d607d65e0a0
sarahjstrong <artbysarahjessicastrong@gmail.com>
2024-04-12 14:31:59 -0400

    updated database scheme to include user_favorites table so a user can favorite messages they receive

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -2,6 +2,7 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users_favorites;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -18,6 +19,15 @@ CREATE TABLE messages (
   message text NOT NULL,
   CONSTRAINT PK_message PRIMARY KEY (message_id),
   CONSTRAINT FK_message_username FOREIGN KEY (message_username) REFERENCES users(username)
+);
+
+CREATE TABLE users_favorites (
+    users_favorites_id SERIAL,
+    user_id integer,
+    message_id integer,
+    CONSTRAINT PK_users_favorites PRIMARY KEY (users_favorites_id),
+    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT FK_message_id FOREIGN KEY (message_id) REFERENCES messages(message_id)
 );
 
 COMMIT TRANSACTION;
